98f30760d5dd0864511b919f11fa0603a139b889
alfredopv <wafleshark1995@hotmail.com>
2018-05-14 01:41:39 -0500

    Pueblos mágicos añadidos a la base de datos automáticamente

--- API/src/main/java/com/example/restapi/RestapiApplication.java
+++ API/src/main/java/com/example/restapi/RestapiApplication.java
@@ -1,6 +1,17 @@
 package com.example.restapi;
 
+import com.example.restapi.model.Pueblo;
 import com.example.restapi.repository.PuebloRepository;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -19,7 +30,11 @@ public class RestapiApplication implements CommandLineRunner {
 	}
 
 	public static void main(String[] args) {
+		
+		
 		SpringApplication.run(RestapiApplication.class, args);
+		
+		
 	}
 
 	@Override
@@ -30,6 +45,34 @@ public class RestapiApplication implements CommandLineRunner {
 		testProduct.setDescripcion("Pueblo Mégico Hermoso");
 		testProduct.setEstado("Michoacán");
 		productRepository.save(testProduct);*/
+		
+		FileReader fileReader;
+		BufferedReader reader;
+		try {
+			fileReader = new FileReader("pueblosmagicos.csv");
+			reader = new BufferedReader(fileReader);
+			String line = null;
+			int index = 1;
+			while((line = reader.readLine()) != null) {
+				String [] datos = line.split(",");
+				Pueblo pueblo = new Pueblo();
+				pueblo.setId(index + "");
+				pueblo.setNombre(datos[1]);
+				pueblo.setEstado(datos[0]);
+				pueblo.setDescripcion("Sin Descripcion");
+				puebloRepository.save(pueblo);
+				//System.out.println(datos[0] + " " + datos[1]);
+				index++;
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 	}
 }

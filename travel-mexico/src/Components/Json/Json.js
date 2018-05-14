71580022b90de08f9e08140eba67d2bca0506def
EmilianoCervantes <ovman96@hotmail.com>
2018-05-14 13:50:11 -0500

    Cambio a axios

--- travel-mexico/src/Components/Json/Json.js
+++ travel-mexico/src/Components/Json/Json.js
@@ -3,51 +3,47 @@
  * Aqui se manda el url para el Distance Matrix API
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 
 const origen = '';
 const destinos = [];
-const ObtenerJson = (props) => {
+class Json extends Component {
+    constructor(props){
+        super(props);
+        this.state = { puebloInicial, puebloFinal, datosDMA };
+    }
     //
     //Un string con todos los destinos como lo pide DMA
     //
-    let urlDestinos = '';
-    /*for (const destino in destinos) {
-        urlDestinos += destino + '|';
-    }*/
-    for(let i=0; i<destinos.length;i++){
-        urlDestinos += destinos[i] + '|';
-    }
-    //Peticion para obtener el json
-    //Nota: xml no significa que solo sea para xml
-    let xmlHttp = new XMLHttpRequest();
-    let urlPeticion = "https://maps.googleapis.com/maps/api/distancematrix/json?origins="+origen+"&destinations="+urlDestinos+"&key=AIzaSyB2vj6e3SsNUFPhGCEF9dazLKMWLAKYP3c";
-    let respuesta;
-    let respuestaParse;
-    xmlHttp.onreadystatechange = () => {
-        if (this.readyState === 4 && this.status === 200) {
-            respuesta = this.responseText;
-            respuestaParse = JSON.parse(this.responseText);
+    componentWillMount(){
+        origen = this.props.match.params.origen;
+        destinos = this.props.match.params.destinos;
+        let urlDestinos = '';
+        /*for (const destino in destinos) {
+            urlDestinos += destino + '|';
+        }*/
+        for(let i=0; i<destinos.length;i++){
+            urlDestinos += destinos[i] + '|';
         }
-    };
-    //XMLHttpRequest.open(method, url, async, user, password)
-    xmlHttp.open("GET",urlPeticion,true);
-    xmlHttp.send();
+        axios.get("https://maps.googleapis.com/maps/api/distancematrix/json?origins="+origen+"&destinations="+urlDestinos+"&key=AIzaSyB2vj6e3SsNUFPhGCEF9dazLKMWLAKYP3c")
+        .then(res => {
+            this.setState( { datosDMA:res.data } );
+        }
+        )
 
-    /**
-     * Aqui ya podemos manipular los campos de respuesta
-     */
-    console.log(respuesta);         //Esto trae el json entero
-    //Asi se acceden a c/u de los datos
-    console.log(respuestaParse.rows[0].elements[0].distance.text); //Ej: "217 km"
-    console.log(respuestaParse.rows[0].elements[0].distance.value); //Ej: "217361" (esto es en metros)
-    console.log(respuestaParse.rows[0].elements[0].duration.text); //Ej: "2 h 20 min"
-    console.log(respuestaParse.rows[0].elements[0].duration.value); //Ej: "8381" (esto es en segundos)
+        console.log(datosDMA);
+        console.log(datosDMA.rows[0].elements[0].distance.text); //Ej: "217 km"
+        console.log(datosDMA.rows[0].elements[0].distance.value); //Ej: "217361" (esto es en metros)
+        console.log(datosDMA.rows[0].elements[0].duration.text); //Ej: "2 h 20 min"
+        console.log(datosDMA.rows[0].elements[0].duration.value); //Ej: "8381" (esto es en segundos)
+    }
 
     //Tags pre y code sirven para mostrar un json
-    return (
-        <pre className="json"><code></code></pre>
-    )
+    render () {
+        return (
+            <pre className="json"><code></code></pre>
+        );
+    }
 }
 
-export default ObtenerJson;
+export default Json;

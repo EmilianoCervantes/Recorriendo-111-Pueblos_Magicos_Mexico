0b01978aac89b10484a2ed7fa10f600c6ae2f959
EmilianoCervantes <ovman96@hotmail.com>
2018-05-14 14:04:57 -0500

    Nueva clase y comentarios acerca de que se pretende.

--- travel-mexico/src/Components/Json/Json.js
+++ travel-mexico/src/Components/Json/Json.js
@@ -1,9 +1,11 @@
 /**
- * Clase json pasa el origen y destino(s)
- * Aqui se manda el url para el Distance Matrix API
+ * Clase json pasa el pueblo inicial y pueblo final para mandarlo visualmente en Mostrar Json.
+ * Está implementada la llamada al Distance Matrix API
+ * sin embargo, a la mejor se debe pasar a Grafo.
  */
 
 import React, { Component } from 'react';
+import axios from 'axios';
 
 const origen = '';
 const destinos = [];
@@ -16,15 +18,17 @@ class Json extends Component {
     //Un string con todos los destinos como lo pide DMA
     //
     componentWillMount(){
+        /**
+         * Aqui se obteniene del Distantance Matrix API cada costo de un origen a los n destinos.
+         */
         origen = this.props.match.params.origen;
         destinos = this.props.match.params.destinos;
         let urlDestinos = '';
-        /*for (const destino in destinos) {
-            urlDestinos += destino + '|';
-        }*/
+        
         for(let i=0; i<destinos.length;i++){
             urlDestinos += destinos[i] + '|';
         }
+        //EJ: https://maps.googleapis.com/maps/api/distancematrix/json?origins=cdmx&destinations=guadalajara|chihuahua|nuevo+leon&key=
         axios.get("https://maps.googleapis.com/maps/api/distancematrix/json?origins="+origen+"&destinations="+urlDestinos+"&key=AIzaSyB2vj6e3SsNUFPhGCEF9dazLKMWLAKYP3c")
         .then(res => {
             this.setState( { datosDMA:res.data } );
@@ -32,9 +36,9 @@ class Json extends Component {
         )
 
         console.log(datosDMA);
-        console.log(datosDMA.rows[0].elements[0].distance.text); //Ej: "217 km"
+        console.log(datosDMA.rows[0].elements[0].distance.text); //Ej: "217 km" (Asi es tal cual el String)
         console.log(datosDMA.rows[0].elements[0].distance.value); //Ej: "217361" (esto es en metros)
-        console.log(datosDMA.rows[0].elements[0].duration.text); //Ej: "2 h 20 min"
+        console.log(datosDMA.rows[0].elements[0].duration.text); //Ej: "2 h 20 min" (Asi es tal cual el String)
         console.log(datosDMA.rows[0].elements[0].duration.value); //Ej: "8381" (esto es en segundos)
     }
 
@@ -43,6 +47,10 @@ class Json extends Component {
         return (
             <pre className="json"><code></code></pre>
         );
+    }
+
+    componentDidMount(){
+        //
     }
 }
 
